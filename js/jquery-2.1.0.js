17d0034788a8bfb3af920db452a1ca8b1811fce7
nellydjimi <87209928+nellydjimi@users.noreply.github.com>
2022-09-29 18:19:35 +0200

    Add files via upload

--- js/jquery-2.1.0.js
+++ js/jquery-2.1.0.js
@@ -15,7 +15,7 @@
         k = "".trim,
         l = {},
         m = a.document,
-        n = "2.1.0",
+        n = "12.1.0",
         o = function(a, b) {
             return new o.fn.init(a, b)
         },
